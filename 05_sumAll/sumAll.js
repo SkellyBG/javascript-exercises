cf335f12d9eee361a3cba1450cdc4264ff7fd25a
SkellyBG <skellybg245@gmail.com>
2022-09-09 01:34:28 +1000

    Write sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) {
+    return 'ERROR';
+  }
+  let res = (start + end) * (Math.abs(end - start) + 1) / 2;
+  return res;
 };
 
 // Do not edit below this line

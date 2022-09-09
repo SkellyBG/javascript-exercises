b50b1a225f67b37dd867829437bc1a846e506e97
SkellyBG <skellybg245@gmail.com>
2022-09-09 23:06:58 +1000

    Write tempConversion function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp = (temp - 32) / 1.8;
+  temp = +temp.toFixed(1);
+  return temp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = temp * 1.8 + 32;
+  temp = +temp.toFixed(1);
+  return temp;
 };
 
 // Do not edit below this line

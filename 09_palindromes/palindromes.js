0913837e87aa2624398398c51845c180fbc7e47e
SkellyBG <skellybg245@gmail.com>
2022-09-11 22:59:25 +1000

    Write palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function(string) {
+  let formatted = "";
 
+  //parse, removing all non-alphabet character and add the lowercase version to formatted string
+  for (let i = 0; i < string.length; i++) {
+    let c = string.charAt(i);
+    if (c.match(/[a-zA-Z]/)) {
+      formatted += c.toLowerCase();
+    }
+  }
+
+  let left = 0;
+  let right = formatted.length - 1;
+  while (left < right) {
+    if (formatted.charAt(left) !== formatted.charAt(right)) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
 };
 
 // Do not edit below this line

9d14bb9feec83f73f884b7c63427eb92f1c565fc
SkellyBG <skellybg245@gmail.com>
2022-09-11 23:06:07 +1000

    Write fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+  member = Number.parseInt(member);
+  if (member <= 0) {
+    return "OOPS";
+  }
+  let prev = 1;
+  let cur = 1;
+  for (let i = 2; i < member; i++) {
+    let temp = prev;
+    prev = cur;
+    cur = cur + temp;
+  }
+  return cur;
 };
 
 // Do not edit below this line

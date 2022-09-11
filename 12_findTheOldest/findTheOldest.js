742af9d586562d74236aa7c390104bb8820b5df3
SkellyBG <skellybg245@gmail.com>
2022-09-11 23:26:32 +1000

    Write findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  let oldest = array[0];
+  for (const person of array) {
+    if (getAge(person) > getAge(oldest)) {
+      oldest = person;
+    }
+  }
+  return oldest;
 };
+
+const getAge = function(person) {
+  let birthYear = person.yearOfBirth;
+  let deathYear = person.yearOfDeath;
+  if (deathYear === undefined) {
+    deathYear = new Date().getFullYear();
+  }
+  return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;

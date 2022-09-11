67d22ec9a2def1005fb5122fd7a846b14fab4a37
SkellyBG <skellybg245@gmail.com>
2022-09-11 23:11:11 +1000

    Write getTheTitles function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+  let res = [];
+  for (const book of array) {
+    res.push(book.title);
+  }
+  return res;
 };
 
 // Do not edit below this line
